05e121e379b4cede06d1ec81338c3d2dcc96cc75
Jarred <jarred.masterson@gmail.com>
2023-12-30 20:00:55 -0700

    Here's day 6 part 1. I'm pretty happy with this solution

---
+++ 06/1.py
@@ -0,0 +1,46 @@
+from numpy import sqrt, prod
+from math import ceil, floor
+
+input_data = []
+answer = 0
+answer_parts = []
+race_data = []
+
+with open('input') as file:
+    for line in file:
+        input_data.append(line)
+
+for line_index, line in enumerate(input_data):
+    # print(line)
+    line_contents = line.split()
+    if line_contents[0] == 'Time:':
+        for line_element in line_contents:
+            if line_element == 'Time:':
+                continue
+            race_data.append([line_element])
+    elif line_contents[0] == 'Distance:':
+        for index, line_element in enumerate(line_contents):
+            if line_element == 'Distance:':
+                continue
+            race_data[index - 1].append(line_element)
+
+for race in race_data:
+    race_record = int(race[1])
+    race_ms = int(race[0])
+    # equation to find race_distance = (race_ms - charge_ms) * charge_ms
+    lower_range = .5 * (race_ms - sqrt(race_ms ** 2 - 4 * race_record))
+    if float.is_integer(lower_range):
+        lower_range = int(lower_range) + 1
+    else:
+        lower_range = ceil(lower_range)
+
+    upper_range = .5 * (sqrt(race_ms ** 2 - 4 * race_record) + race_ms)
+    if float.is_integer(upper_range):
+        upper_range = int(upper_range) - 1
+    else:
+        upper_range = floor(upper_range)
+
+    answer_parts.append(len(range(lower_range, upper_range+1)))
+
+answer = prod(answer_parts)
+print(answer)
